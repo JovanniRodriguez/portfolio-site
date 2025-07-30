792f5a677d20e0afb77818aae0f53d6df915156c
jovyp <jovips1@gmail.com>
2025-07-30 18:38:29 -0400

    ignore

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<Home/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "K2D", system-ui;
+  font-weight: 400;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
---
+++ project/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      
+      <!--start carousel component-->
+      <div class="col" id="left-carousel-col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nostrum sapiente natus quas fuga labore voluptatum at eos qui delectus?</div>
+      <!--end carousel component-->
+
+      <!--start info col-->
+      <div class="col" id="right-info-col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis doloribus, possimus exercitationem voluptatum inventore soluta ipsa aliquam ex maiores est?</div>
+      <!--end info col-->
+
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#right-info-col {
+  background-color: black;
+  color: white;
+}
+</style>
