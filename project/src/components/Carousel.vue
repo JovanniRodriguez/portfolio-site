65179a84bf4303d3de24b4a4b6ae98c5cfad4759
jovyp <jovips1@gmail.com>
2025-08-01 23:51:13 -0400

    ignore

--- project/src/components/Carousel.vue
+++ project/src/components/Carousel.vue
@@ -3,7 +3,7 @@
 
 
         <div class="carousel slide" id="carouselControls">
-            <div class="carousel inner">
+            <div class="carousel-inner">
                 <!--Title card-->
                 <div class="carousel-item active">
                     <div class="card">
@@ -13,15 +13,15 @@
                                     <h1 class="display-1">Web & Software <br> Developer</h1>
                                 </div>
                             </div>
-                            <div class="row">
+                            <div class="row bg-danger">
                                 <div class="col">
-                                    <img class="nav-arrow-right-img" src="/icons/nav-arrow-right-icon.png" alt="next">
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!--Title card end-->
+                <!--end Title card-->
                 <!--Experience card-->
                 <div class="carousel-item">
                     <div class="card">
@@ -30,8 +30,18 @@
                         </div>
                     </div>
                 </div>
+                <!--end Experience card-->
             </div>
         </div>
+
+        <!-- <div class="row">
+            <div class="col">
+                <button role="button" data-bs-slide="next"
+                data-bs-target="#carouselControls" id="nextSlideButton">
+                    <img class="nav-arrow-right-img" src="/icons/nav-arrow-right-icon.png" alt="next">
+                </button>
+            </div>
+        </div> -->
 
     </div>
 </template>
@@ -51,5 +61,14 @@ export default {
 .nav-arrow-right-img:hover {
     transform: scale(1.3);
     transition: transform 0.5s ease;
+}
+
+#nextSlideButton {
+    border: none;
+    border-radius: 40%;
+    width: auto;
+    height: fit-content;
+    background-color: transparent;
+    cursor:auto;
 }
 </style>
--- project/src/components/Home.vue
+++ project/src/components/Home.vue
@@ -3,11 +3,21 @@
     <div class="row h-100">
 
       <!--start carousel component-->
-      <div class="col d-flex h-100 justify-content-center align-items-center text-center" id="left-carousel-col">
+      <div class="col d-flex flex-column h-100 justify-content-center align-items-center text-center" id="left-carousel-col">
         <TitleSlide>
 
         </TitleSlide>
+        <div class="row">
+          <div class="col">
+            <button role="button" data-bs-slide="next"
+            data-bs-target="#carouselControls" id="nextSlideButton">
+            	<img class="nav-arrow-right-img" src="/icons/nav-arrow-right-icon.png" alt="next">
+            </button>
+          </div>
+        </div>
       </div>
+      
+      
       <!--end carousel component-->
 
       <!--start info col-->
@@ -83,5 +93,24 @@ export default {
 .contact-icon:hover {
   transform: scale(1.3);
   transition: transform 0.5s ease;
+}
+
+.nav-arrow-right-img {
+    width: 70px;
+    cursor: pointer;
+}
+
+.nav-arrow-right-img:hover {
+    transform: scale(1.3);
+    transition: transform 0.5s ease;
+}
+
+#nextSlideButton {
+    border: none;
+    border-radius: 40%;
+    width: auto;
+    height: fit-content;
+    background-color: transparent;
+    cursor:auto;
 }
 </style>
