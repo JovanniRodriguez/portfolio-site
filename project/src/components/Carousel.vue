d264f010caf299c9c10d100e72bcbe94bfb6f6dc
jovyp <jovips1@gmail.com>
2025-08-02 01:02:58 -0400

    added all slides, have them sliding. Need to keep slide text from overflowing and hiding on certain viewports. Some text gets hidden behind the slide instead of breaking to the next line. Need to add previous nav arrow. Need to fix anchor tag on projects slide from taking up full width.

--- project/src/components/Carousel.vue
+++ project/src/components/Carousel.vue
@@ -15,7 +15,7 @@
                             </div>
                             <div class="row bg-danger">
                                 <div class="col">
-                                    
+
                                 </div>
                             </div>
                         </div>
@@ -26,11 +26,115 @@
                 <div class="carousel-item">
                     <div class="card">
                         <div class="card-body">
-                            <h1>experience card test</h1>
+                            <h1 class="display-1 text-start mb-5">Experience</h1>
+                            <div class="container mx-5">
+                                <h1 class="text-start">
+                                    <strong class="fw-bold">
+                                        What:
+                                    </strong>
+                                    Web Developer/Designer
+                                </h1>
+                                <h1 class="text-start">
+                                    <strong class="fw-bold">
+                                        Where:
+                                    </strong>
+                                    Eastman Kodak (remote)
+                                </h1>
+                                <h1 class="text-start">
+                                    <strong class="fw-bold">
+                                        When:
+                                    </strong>
+                                    May 2024 - Aug 2024
+                                </h1>
+
+                                <ul class="mx-5 mt-4">
+                                    <li class="exp-bulletpoint text-start">
+                                        <h4>
+                                            Improved user experience and helped with
+                                            a full site redesign using Vue.js
+                                        </h4>
+
+                                    </li>
+                                    <li class="exp-bulletpoint text-start">
+                                        <h4>
+                                            Worked independently and in collaboration with a team
+                                        </h4>
+                                    </li>
+                                    <li class="exp-bulletpoint text-start">
+                                        <h4>
+                                            Improved overall site functionality and QA testing
+                                        </h4>
+                                    </li>
+                                    <li class="exp-bulletpoint text-start">
+                                        <h4>
+                                            Researched and recommended improvements for the site
+                                        </h4>
+                                    </li>
+                                    <li class="exp-bulletpoint text-start">
+                                        <h4>
+                                            Updated page content through a Content Management System (CMS)
+                                        </h4>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+
+
                         </div>
                     </div>
                 </div>
                 <!--end Experience card-->
+                <!--Education card-->
+                <div class="carousel-item">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1 class="display-1 text-start mb-5">Education</h1>
+
+                            <div class="container mx-5">
+                                <h1 class="text-start fw-bold">Bachelor of Science</h1>
+                                <h3 class="text-start ms-5">in Computer Science</h3>
+                                <h3 class="text-start ms-5">Florida Southern College</h3>
+                                <h3 class="text-start ms-5">Aug 2022 - May 2025</h3>
+                            </div>
+
+                            <div class="container mt-5 mx-5">
+                                <h1 class="text-start fw-bold">Associate of Arts</h1>
+                                <h3 class="text-start ms-5">Hillsborough Community College</h3>
+                                <h3 class="text-start ms-5">May 2017 - Dec 2021</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--end Education card-->
+                <!--Recent projects card-->
+                <div class="carousel-item">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1 class="display-1 text-start mb-5">Recent Projects</h1>
+
+                            <div class="container mx-5">
+                                <h1 class="text-start fw-bold">Nebulock</h1>
+                                <h3 class="text-start ms-5">Vue.js, Firebase, Git, HTML/CSS, JS</h3>
+                                <h3 class="text-start ms-5">Complex full-stack web application 
+                                    that allows users to manage items in the game Destiny 2</h3>
+                            </div>
+
+                            <div class="container mt-5 mx-5 mb-5">
+                                <h1 class="text-start fw-bold">Flashdeck</h1>
+                                <h3 class="text-start ms-5">Django, Python, HTML/CSS, JS</h3>
+                                <h3 class="text-start ms-5">full-stack web application for 
+                                    creating and studying flashcards with advanced memorization 
+                                    techniques</h3>
+                            </div>
+
+                            <a class="repo-link" href="https://github.com/JovanniRodriguez?tab=repositories">
+                                <h2 class="text-start mt-5 ms-2">More Projects</h2>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!--end Recent projects card-->
             </div>
         </div>
 
@@ -53,6 +157,9 @@ export default {
 </script>
 
 <style scoped>
+.card-body{
+    height: 800px;
+}
 .nav-arrow-right-img {
     width: 70px;
     cursor: pointer;
@@ -69,6 +176,22 @@ export default {
     width: auto;
     height: fit-content;
     background-color: transparent;
-    cursor:auto;
+    cursor: auto;
+}
+
+.exp-bulletpoint {
+    word-wrap: break-word;
+    white-space: normal;
+    overflow-wrap: anywhere;
+}
+
+.repo-link {
+    opacity: 10%;
+    text-decoration: underline;
+    color: black;
+}
+.repo-link:hover {
+    opacity: 100%;
+    transition: 
 }
 </style>
